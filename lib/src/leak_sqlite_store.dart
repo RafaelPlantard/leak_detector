e4419ac0dbafada6dd2edf4f7f3445da2ccaa240
Rafael da Silva Ferreira <2582032-RafaelPlantard@users.noreply.gitlab.com>
2025-03-06 17:05:36 -0300

    feat: add SQLite support for Android with proper path handling

--- lib/src/leak_sqlite_store.dart
+++ lib/src/leak_sqlite_store.dart
@@ -6,13 +6,14 @@ import 'dart:io';
 
 import 'package:leak_detector/src/leak_data.dart';
 import 'package:leak_detector/src/leak_data_store.dart';
-import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:sqlite3/sqlite3.dart';
 
 ///database
 class _LeakDataBase {
-  static Database _openDatabase() {
-    final dbPath = join(Directory.current.path, 'leak_recording.db');
+  static Future<Database> _openDatabase() async {
+    final appDir = await getApplicationDocumentsDirectory();
+    final dbPath = '${appDir.path}/leak_recording.db';
     final db = sqlite3.open(dbPath);
 
     db.execute(
@@ -37,16 +38,25 @@ class _LeakRecordingTable {
 ///[_LeakRecordingTable] Helper
 class LeakedRecordSQLiteStore implements LeakedRecordStore {
   static LeakedRecordSQLiteStore? _instance;
-  final Database _db;
+  late final Database _db;
+  bool _initialized = false;
 
   factory LeakedRecordSQLiteStore() {
     _instance ??= LeakedRecordSQLiteStore._();
     return _instance!;
   }
 
-  LeakedRecordSQLiteStore._() : _db = _LeakDataBase._openDatabase();
+  LeakedRecordSQLiteStore._();
+
+  Future<void> _ensureInitialized() async {
+    if (!_initialized) {
+      _db = await _LeakDataBase._openDatabase();
+      _initialized = true;
+    }
+  }
 
   Future<List<LeakedInfo>> _queryAll() async {
+    await _ensureInitialized();
     final ResultSet resultSet = _db.select(
       'SELECT * FROM ${_LeakRecordingTable._kTableName}',
     );
@@ -55,6 +65,7 @@ class LeakedRecordSQLiteStore implements LeakedRecordStore {
   }
 
   Future<void> _insert(LeakedInfo data) async {
+    await _ensureInitialized();
     _db.execute(
       'INSERT OR REPLACE INTO ${_LeakRecordingTable._kTableName} '
       '(${_LeakRecordingTable._kId}, ${_LeakRecordingTable._kGCRootType}, ${_LeakRecordingTable._kLeakPathJson}) '
@@ -64,6 +75,7 @@ class LeakedRecordSQLiteStore implements LeakedRecordStore {
   }
 
   Future<void> _insertAll(List<LeakedInfo> data) async {
+    await _ensureInitialized();
     final stmt = _db.prepare(
       'INSERT OR REPLACE INTO ${_LeakRecordingTable._kTableName} '
       '(${_LeakRecordingTable._kId}, ${_LeakRecordingTable._kGCRootType}, ${_LeakRecordingTable._kLeakPathJson}) '
@@ -78,6 +90,7 @@ class LeakedRecordSQLiteStore implements LeakedRecordStore {
   }
 
   Future<void> _deleteById(int id) async {
+    await _ensureInitialized();
     _db.execute(
       'DELETE FROM ${_LeakRecordingTable._kTableName} WHERE ${_LeakRecordingTable._kId} = ?',
       [id.toString()],
@@ -85,6 +98,7 @@ class LeakedRecordSQLiteStore implements LeakedRecordStore {
   }
 
   Future<void> _deleteAll() async {
+    await _ensureInitialized();
     _db.execute('DELETE FROM ${_LeakRecordingTable._kTableName}');
   }
 
